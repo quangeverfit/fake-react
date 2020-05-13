fa954f0991371a456b678eea5539004a91efbeac
hieumai-everfit <64888035+hieumai-everfit@users.noreply.github.com>
2020-05-13 17:59:38 +0700

    Update push.sh

--- ci/push.sh
+++ ci/push.sh
@@ -10,7 +10,7 @@ docker tag everfit-demo-frontend:latest "$IMAGE_URL_WITHOUT_VER:latest"
 docker push "$IMAGE_URL_WITHOUT_VER:latest"
 
 # Tag image with specfic version
-docker tag everfit-demo-frontend:latest "$IMAGE_URL_WITHOUT_VER:3"
+docker tag everfit-demo-frontend:latest "$IMAGE_URL_WITHOUT_VER:3.1234567890"
 
 # Push image with specfic version
-docker push "$IMAGE_URL_WITHOUT_VER:3"
+docker push "$IMAGE_URL_WITHOUT_VER:3.1234567890"
